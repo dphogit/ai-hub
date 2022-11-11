636c15d5d232faddf381ff0e80440334914c9e81
Dean Phommahaxay <deanp21@gmail.com>
2022-11-11 22:44:58 +1300

    Add ESLint and Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { Heading } from '@chakra-ui/react'
+import { Heading } from '@chakra-ui/react';
 
 const App = () => {
-  return <Heading>Hello World!</Heading>
-}
+  return <Heading>Hello World!</Heading>;
+};
 
 export default App;
