c24679abef791a742bfb3ad3dd74d3d66796905a
Dean Phommahaxay <deanp21@gmail.com>
2022-11-12 17:59:26 +1300

    Add React Router dependency

---
+++ src/components/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import Layout from '../../layout';
+import { Heading } from '@chakra-ui/react';
+
+const Home = () => {
+  return (
+    <Layout>
+      <Heading>Hello World! This is the home page!</Heading>
+    </Layout>
+  );
+};
+
+export default Home;
